55053c6e337c748fdd29a0768a77400898273222
Muhammad Jayyousi <33544679+Jayousim@users.noreply.github.com>
2020-12-01 12:32:33 +0200

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-TOKEN = '1465334834:AAFTxfNe4rR0lxx0TQnsuvgZNZzCIowLB_E'
-server_ip = 'https://76abed448dca.ngrok.io'
-TELEGRAM_INIT_WEBHOOK_URL = 'https://api.telegram.org/bot{}/setWebhook?url={}/message'.format(TOKEN, server_ip)
-
-requests.get(TELEGRAM_INIT_WEBHOOK_URL)
-
-GOOGLE_KEY = "AIzaSyBONgiulyX32tzphZd1Iti5VbfNpi5L_0k"

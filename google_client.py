197cce425cc26b36f8af8646ce8648e58344c335
Marwan Bushara <43731624+mah3ot95@users.noreply.github.com>
2020-12-01 10:58:29 +0200

    updated google_client

--- google_client.py
+++ google_client.py
@@ -1,5 +1,5 @@
 import time
-from flask import Flask,request
+from flask import Flask, request
 import requests
 from config import GOOGLE_KEY
 
@@ -8,7 +8,8 @@ class GoogleApiInvoker:
     key = GOOGLE_KEY
     textsearch = "https://maps.googleapis.com/maps/api/place/textsearch/json?"
     nearbysearch = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
-    @staticmethod
+    radius = 1000
+    MAX_HOTELS = 5
 
     @staticmethod
     def get_street_photos(loacation):
@@ -19,24 +20,45 @@ class GoogleApiInvoker:
         pass
 
     @staticmethod
-    def get_all_hotels(destination):
+    def get_hotels(destination):
         hotels_query = f"hotels in {destination}"
         ans = requests.get(f"{GoogleApiInvoker.textsearch}key={GoogleApiInvoker.key}&query={hotels_query}").json()
         hotels = ans.get("results")
-        while ans.get("next_page_token"):
-            time.sleep(2)
-            ans = requests.get(f"{GoogleApiInvoker.textsearch}key={GoogleApiInvoker.key}&pagetoken={ans.get('next_page_token')}").json()
-            hotels += ans.get("results")
+        hotels += GoogleApiInvoker.get_next_hotels(ans.get('next_page_token'))
         return hotels
 
     @staticmethod
-    def get_by_keyword(keyword):
+    def get_next_hotels(next_page):
+        if not next_page:
+            return []
+        result = requests.get(f"{GoogleApiInvoker.textsearch}key={GoogleApiInvoker.key}"
+                              f"&pagetoken={next_page}").json()
+        return result.get("results") + GoogleApiInvoker.get_next_hotels(result.get('next_page_token'))
+
+    @staticmethod
+    def get_location_from_json(data):
+
+        return data.get('geometry').get('location').get('lat'), data.get('geometry').get('location').get('lng')
+
+    @staticmethod
+    def get_next_activities(next_page):
+        if not next_page:
+            return []
+        result = requests.get(f"{GoogleApiInvoker.nearbysearch}key={GoogleApiInvoker.key}"
+                              f"&pagetoken={next_page}").json()
+        return result.get("results") + GoogleApiInvoker.get_next_activities(result.get('next_page_token'))
+
+    @staticmethod
+    def get_activities_by_hotel(hotel, activity):
+        lat, lng = GoogleApiInvoker.get_location_from_json(hotel)
         ans = requests.get(
-            f"{nearbysearch}key={key}&location={hotel_lats[i]},{hotel_lngs[i]}&radius={radius}&keyword={keyword}").json()
+            f"{GoogleApiInvoker.nearbysearch}key={GoogleApiInvoker.key}&location={lat},{lng}"
+            f"&radius={GoogleApiInvoker.radius}&keyword={activity}").json()
         current_results = ans.get("results")
         while ans.get("next_page_token"):
             time.sleep(2)
-            ans = requests.get(f"{nearbysearch}key={key}&pagetoken={ans.get('next_page_token')}").json()
+            ans = requests.get(
+                f"{GoogleApiInvoker.nearbysearch}key={GoogleApiInvoker.key}"
+                f"&pagetoken={ans.get('next_page_token')}").json()
             current_results += ans.get("results")
         return [item.get('name') for item in current_results]
-

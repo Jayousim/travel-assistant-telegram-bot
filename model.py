19aed16c70e15b183995d2b968360cdcf250927b
Muhammad Jayyousi <mhmd.t2@gmail.com>
2020-12-01 11:58:09 +0200

    fix branch

---
+++ model.py
@@ -0,0 +1,15 @@
+class SearchEngine:
+    def find_top_stays_with_type(self, activity_type):
+        pass
+
+    def get_more_hotels_neraby(self):
+        pass
+
+    @staticmethod
+    def get_street_photos(location):
+        pass
+
+    @staticmethod
+    def get_place_description(place):
+        pass
+
